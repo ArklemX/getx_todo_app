fd2ecb36131faa540d9d876b42a555ff8f71a6f3
ArklemX <avaikamarcel@gmail.com>
2023-03-10 22:47:47 +0100

    add tasks repository

--- lib/app/data/services/storage/repository.dart
+++ lib/app/data/services/storage/repository.dart
@@ -1,1 +1,12 @@
+import 'package:getx_todo_app/app/data/providers/task/providers/task_provider.dart';
 
+import '../../models/task.dart';
+
+class TaskRepository {
+  TaskProvider taskProvider;
+  TaskRepository({required this.taskProvider});
+
+  List<Task> readTasks() => taskProvider.readTasks();
+
+  void writeTasks(List<Task> tasks) => taskProvider.writeTask(tasks);
+}
