e5a9e032be6b811cb62b07f2d4515f008c13a2bc
ArklemX <avaikamarcel@gmail.com>
2023-03-10 21:57:50 +0100

    add task storage service

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:getx_todo_app/app/data/services/storage/repository.dart';
 
 import 'app/routes/app_pages.dart';
 
 Future<void> main() async {
   await GetStorage.init();
+  await Get.putAsync(() => StorageService().init());
   runApp(
     GetMaterialApp(
       title: "Application",
