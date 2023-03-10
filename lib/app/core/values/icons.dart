6273a6a265e99c1d2eeacd5745fbe2f4a0af27de
ArklemX <avaikamarcel@gmail.com>
2023-03-10 21:32:41 +0100

    add icons file

---
+++ lib/app/core/values/icons.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const personIcon = 0xe491; //Icons.person
+const workIcon = 0xe11c; //Icons.work
+const movieIcon = 0xe40f; //Icons.movie
+const sportIcon = 0xe4dc; //Icons.sports
+const travelIcon = 0xe071; // Icons.travel_explore
+const shopIcon = 0xe59c; //Icons.shop
